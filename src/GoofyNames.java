f5f2659bd745feb8ff55d5248e5f33202077691c
varnit05 <league@wts13.attlocal.net>
2018-05-01 16:58:38 -0700

    Circle Ring, goofy names, guessing pi, for loop gauntlet IP

--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -18,7 +18,23 @@ System.out.println(name.toUpperCase());
 
 		// 3. Loop through each character of the name (steps 4 - 6).
 		//    HINT: Use .length() to determine the number of characters in the String.
+for (int i = 0; i < name.length(); i++) {
+	System.out.println(name.charAt(i));
+	//.length() is number of characters in the word. Because you start counting from 0,
+	//i stops at the digit before the length number. charAt(i) prints the number
+	//located in position i, or whatever is put in the parenthesis
 
+	String output = "";
+String character= name.substring(i,i+1); //creates a new string before the first digit (0) and before the next (1). other numbers can be put here.
+										//You want to chop every letter so you put i and i+1 for each time i changes.
+if (i%2 == 0) {
+	output = character.toUpperCase(); //always save information into a string variable
+	
+}
+else {
+	output = character.toLowerCase();
+}
+goofyName += output;  //step 6 --- add the output to goofyName and make it equal to
 			// 4. Create a String variable to store the next character of the name
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
@@ -29,9 +45,9 @@ System.out.println(name.toUpperCase());
 			
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
-		
+}
 		// 7. Use pop-up to show user their Goofy name
-
+System.out.println(goofyName);
 	}
 }
 
